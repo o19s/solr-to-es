65add5187993efdfd55192ed0e0f3c660dec9369
Joseph Lawson <joe@joekiller.com>
2015-08-18 17:19:39 -0400

    First version of solr-to-es.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Solr to Elasticsearch Migrator
+
+This will migrate a Solr node to an Elasticsearch index.
+
+## Requirements
+
+  * Python 2.7+
+    * elasticsearch
+    * pysolr
+    
+## Usage
+
+```
+usage: solr-to-es.py [-h] [--solr-query SOLR_QUERY]
+                     [--rows-per-page ROWS_PER_PAGE]
+                     solr_url elasticsearch_url elasticsearch_index doc_type
+```
+
+The following example will page through all documents on the local Solr node, `node`, and submit them to the local Elasticsearch server in the index `es_index` with a document type of `solr_docs`.
+
+```bash
+solr-to-es.py localhost:8983/solr/node localhost:9200 es_index solr_docs
+```
+
+`solr_url` is the url to your Solr node.
+
+`elasticsearch_url` is the url of your Elasticsearch server.
+
+`elasticsearch_index` is the index you will submit the Solr documents to on Elasticsearch.
+
+`doc_type` is the type of document Elasticsearch should assume you are importing.
+
+`--solr-query` defaults to `*:*`
+
+`--rows-per-page` defaults to `100`
---
+++ solr-to-es.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+from elasticsearch import Elasticsearch
+import elasticsearch.helpers
+import pysolr
+import sys
+
+
+class SolrEsWrapperIter:
+    def __init__(self, index, type, solr_itr):
+        self.index = index
+        self.type = type
+        self.solr_itr = iter(solr_itr)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        doc = self.solr_itr.next()
+        new_doc = dict()
+        new_doc['_index'] = self.index
+        new_doc['_type'] = self.type
+        new_doc['_source'] = doc
+        return new_doc
+
+
+class SolrRequestIter:
+    def __init__(self, solr_url, query, rows):
+        self.current = 0
+        self.query = query
+        self.solr_conn = pysolr.Solr(solr_url)
+        self.rows = rows
+        self.max = None
+        self.docs = None
+
+    def __iter__(self):
+        response = self.solr_conn.search(self.query, rows=0)
+        self.max = response.hits
+        return self
+
+    def next(self):
+        if self.docs is not None:
+            try:
+                return self.docs.next()
+            except StopIteration:
+                self.docs = None
+        if self.docs is None:
+            if self.current * self.rows < self.max:
+                self.current += 1
+                response = self.solr_conn.search(self.query, rows=self.rows, start=(self.current - 1) * self.rows)
+                self.docs = iter(response.docs)
+                return self.docs.next()
+            else:
+                raise StopIteration()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('solr_url',
+                        type=str)
+
+    parser.add_argument('--solr-query',
+                        type=str,
+                        default='*:*')
+
+    parser.add_argument('elasticsearch_url',
+                        type=str)
+
+    parser.add_argument('elasticsearch_index',
+                        type=str)
+
+    parser.add_argument('doc_type',
+                        type=str)
+
+    parser.add_argument('--rows-per-page',
+                        type=int,
+                        default=100)
+
+    return vars(parser.parse_args())
+
+
+def main():
+    args = parse_args()
+    es = Elasticsearch(args['elasticsearch_url'])
+    solr_itr = SolrRequestIter(args['solr_url'], args['solr_query'], args['rows_per_page'])
+    elasticsearch.helpers.bulk(es, SolrEsWrapperIter(args['elasticsearch_index'], args['elasticsearch_index'], solr_itr))
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
